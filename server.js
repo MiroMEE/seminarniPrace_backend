33be37b3bdae9b1cd200c8a805014d337384eb60
Mirooooo xd <mirdajakes@gmail.com>
2024-01-14 18:56:51 +0100

    please wrok

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors');
+const uri = 'mongodb+srv://mirdajakes:CMMMkklSX9iczMeH@seminarniprace-app-web.gjqizxi.mongodb.net/'
+const params = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}
 
 app.use(express.json());
 app.use(cors());
@@ -9,15 +14,17 @@ app.use(cors());
 require('./API/slovicka/slovicka')(app);
 require('./API/users/users')(app);
 require('./API/datas/datas')(app);
-mongoose
-    .connect('mongodb://localhost/rj_database',{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    })
-    .then((err)=>{
-        app.listen(3000, () =>{
-            console.log("Node API app is running on port 3000");
-    });
-    }).catch((error)=>{
-        console.log(error);
-    });
+
+async function connect(){
+    try{
+        await mongoose.connect(uri,params);
+        console.log("Connected to MongoDb");
+    } catch (error){
+        console.error(error);
+    }
+}
+connect();
+
+app.listen(3000, () =>{
+    console.log("Node API app is running on port 3000");
+});
